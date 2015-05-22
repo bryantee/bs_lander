f12801751618adab3020ce80c4f5290a170e726d
bryantee <bryantee@gmail.com>
2015-05-22 15:05:17 -0700

    jQuery footer hover icons, awesome-font, cleanup.

--- js/main.js
+++ js/main.js
@@ -3,4 +3,25 @@ $(document).ready(function() {
 	$('.bryan, .swagerty').hide(0).delay(500).fadeIn('slow');
 	$('header > p').hide(0).delay(700).fadeIn('slow');
 
+	$('.twitter').mouseover(function() {
+		console.log('mouseover twitter line');
+		$('.social-icon').children().hide();
+		$('.social1').show();
+	});
+	$('.github').mouseover(function() {
+		console.log('mouseover github line');
+		$('.social-icon').children().hide();
+		$('.social2').show();
+	});
+	$('.instagram').mouseover(function() {
+		console.log('mouseover instagram line');
+		$('.social-icon').children().hide();
+		$('.social3').show();
+	});
+	$('.facebook').mouseover(function() {
+		console.log('mouseover facebook line');
+		$('.social-icon').children().hide();
+		$('.social4').show();
+	});
+
 });
