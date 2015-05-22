f95f591fe169064a81c8ada5615037d7affb5f0e
Bryan Swagerty <bryantee@gmail.com>
2015-05-21 20:09:19 -0700

    Added jQuery animation to header text

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	console.log('documentloaded');
+	$('.bryan, .swagerty').hide(0).delay(500).fadeIn('slow');
+	$('header > p').hide(0).delay(700).fadeIn('slow');
+
+});
